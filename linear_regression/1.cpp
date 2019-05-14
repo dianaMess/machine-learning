bd60caa9837b18228a0e33508abc186f7b8f044e
dianaMess <ospanova2000@mail.ru>
2019-05-14 22:43:43 +0600

    add files

--- linear_regression/1.cpp
+++ linear_regression/1.cpp
@@ -7,10 +7,15 @@ using namespace std;
 class Matrix {
     int row, col;
     vector <vector <double> > data;
-//    string filename;
 public:
     Matrix (int row = 1, int col = 1): row(row), col(col),
            data(vector <vector <double > > (row, vector<double> (col, 0.5))) {}
+    void set(vector <vector <double > > vec) { 
+        for (int i = 0; i < vec.size(); i++)
+            for (int j = 0; j < vec[i].size(); j++)
+                data[i][j] = vec[i][j];
+    }
+    
     int getRow() {
         return row;
     }
@@ -109,8 +114,6 @@ public:
     }
     friend std::istream& operator>>(istream &in, Matrix& mx) {
         double num;
-//        string f = filename;
-//        std::ifstream file("test2.txt");  
         for (int i = 0; i < mx.row; i++) {
             for (int j = 0; j < mx.col; j++) {
                 cin>>num;
@@ -180,16 +183,55 @@ public:
 
 int main(int argc, char * argv[]) {
     Matrix X(8, 7), Y(8, 1), K(5, 7);
-//    std::ifstream file("test2.txt");  
-    cin>>X;
-//    cout<<X;
-    cin>>Y;
+    ifstream file(argv[1]);  
+    ifstream file1(argv[2]);  
+    ifstream file2(argv[3]);  
+
+    double num;
+    vector<vector<double>> X1(8, vector<double>(7));
+    if (file.is_open()) {
+        for (int i = 0; i < 8; i++)
+            for(int j = 0; j < 7; j++) {
+                if (file.eof())
+                    break;
+                file>>num;
+                X1[i][j] = num;
+            }
+    }
+    X.set(X1);
+cout<<X;
+    vector<vector<double>> Y1(8, vector<double>(1));
+    if (file1.is_open()) {
+        for (int i = 0; i < 8; i++)
+            for(int j = 0; j < 1; j++) {
+                if (file1.eof())
+                    break;
+                file1>>num;
+                Y1[i][j] = num;
+            }
+    }
+
+    Y.set(Y1);
+cout<<Y;
+    vector<vector<double>> K1(5, vector<double>(7));
+    if (file2.is_open()) {
+        for (int i = 0; i < 5; i++)
+            for(int j = 0; j < 7; j++) {
+                 if (file2.eof())
+                    break;
+                file2>>num;
+                K1[i][j] = num;
+            }
+    }         
+    K.set(K1);
+cout<<K;
+
     Neuron model(X, Y);
-    cin>>K;
     Matrix A = model.train(X, Y);
     Matrix B = A * K.transpose();
     cout<<endl<<"answer: "<<endl;
     cout<<B;
+
     return 0;
 }
     
