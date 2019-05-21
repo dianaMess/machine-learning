9818944203f0254c4322141042fc40e22aa22b83
dianaMess <ospanova2000@mail.ru>
2019-05-21 21:25:31 +0600

    fix

--- linear_regression/1.cpp
+++ linear_regression/1.cpp
@@ -182,11 +182,11 @@ public:
 };
 
 int main(int argc, char * argv[]) {
-    Matrix X(8, 7), Y(8, 1), K(5, 7);
+    Matrix X(8, 7), Y(8, 1), K(5, 7), Z(5, 1);
     ifstream file(argv[1]);  
     ifstream file1(argv[2]);  
     ifstream file2(argv[3]);  
-
+    ifstream file3(argv[4]);
     double num;
     vector<vector<double>> X1(8, vector<double>(7));
     if (file.is_open()) {
@@ -225,13 +225,25 @@ cout<<Y;
     }         
     K.set(K1);
 cout<<K;
-
     Neuron model(X, Y);
     Matrix A = model.train(X, Y);
     Matrix B = A * K.transpose();
     cout<<endl<<"answer: "<<endl;
     cout<<B;
-
+    cout<<endl<<"right answer: "<<endl;
+    vector<vector<double>> Z1(5, vector<double>(1));
+    if (file3.is_open()) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 1; j++) {
+                if (file3.eof())
+                   break;
+                file3>>num;
+                Z1[i][j] = num;
+            }
+        }
+    }       
+    Z.set(Z1); 
+    cout<<Z; 
     return 0;
 }
     
